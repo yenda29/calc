09ef949620651d74cc8fef66630e558add815b93
Jonáš Herzig <herzigj.03@in.spst.cz>
2023-03-16 19:27:20 +0100

    test commit

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -16,5 +16,10 @@ namespace calc
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ahoj"); 
+        }
     }
 }
